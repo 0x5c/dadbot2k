6b35cc9e3b394d486ef3d35a840e578c7cb4eb4a
0x5c <dev@0x5c.io>
2021-06-03 13:41:00 -0400

    Initial commit

---
+++ templates/data/options.py
@@ -0,0 +1,17 @@
+##########################################################
+#                                                        #
+#                 ¡ This is a template !                 #
+#                                                        #
+#   Make sure to edit it with your preferred settings!   #
+#              And to place it in the /data              #
+#                subdirectory of the bot,                #
+#        without the 'template_' part of the name        #
+#                                                        #
+##########################################################
+"""
+Settings and options for the bot.
+---
+"""
+
+# The prefix for the bot. Define a list of strings for multiple prefixes. ie: `["?", "!", "pls "]`
+command_prefix = "?"
